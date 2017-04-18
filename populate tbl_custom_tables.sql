962c09a4da73a792a2249c9d2ffb14ee8fd4788e
HaleyAndAldrich <dhiggins@haleyaldrich.com>
2017-04-18 16:50:15 -0700

    change to v3

--- populate tbl_custom_tables.sql
+++ populate tbl_custom_tables.sql
@@ -5,19 +5,18 @@ go
 set nocount off
 go
 
-declare @SchemaName varchar (100) = 's_36599_test'
 
 delete tbl_custom_tables
 insert into tbl_custom_tables
-		select 47,'generic','tbl_SE',  'mg/kg', 'SE','N83SPCA III Ft', 'Select * INTO [EQuIS_Reporting].','  FROM [EQuIS].[rpt].[fn_HAI_EQUIS_Results_v2]( @facility_id, @unit, null, @coord_type, @matrix)'
+		select 47,'generic','tbl_SE',  'mg/kg', 'SE','N83SPCA III Ft', 'Select * INTO [EQuIS_Reporting].','  FROM [EQuIS].[rpt].[fn_HAI_EQUIS_Results_v3]( @facility_id, @unit, null, @coord_type, @matrix)'
 		union					
-		select 47,'generic','tbl_SO',  'mg/kg', 'SO','N83SPCA III Ft', 'Select *  INTO [EQuIS_Reporting].',' FROM [EQuIS].[rpt].[fn_HAI_EQUIS_Results_v2]( @facility_id, @unit, null, @coord_type, @matrix)'
+		select 47,'generic','tbl_SO',  'mg/kg', 'SO','N83SPCA III Ft', 'Select *  INTO [EQuIS_Reporting].',' FROM [EQuIS].[rpt].[fn_HAI_EQUIS_Results_v3]( @facility_id, @unit, null, @coord_type, @matrix)'
 		union
-		select  47,'generic','tbl_GW',  'ug/l', 'WG', 'N83SPCA III Ft', 'Select * INTO [EQuIS_Reporting].',' FROM [EQuIS].[rpt].[fn_HAI_EQUIS_Results_v2]( @facility_id, @unit, null, @coord_type, @matrix)'
+		select  47,'generic','tbl_GW',  'ug/l', 'WG', 'N83SPCA III Ft', 'Select * INTO [EQuIS_Reporting].',' FROM [EQuIS].[rpt].[fn_HAI_EQUIS_Results_v3]( @facility_id, @unit, null, @coord_type, @matrix)'
 		union
-		select  47,'generic','tbl_SV',  'mg/m3', 'GS', 'N83SPCA III Ft', 'Select  *  INTO [EQuIS_Reporting].',' FROM [EQuIS].[rpt].[fn_HAI_EQUIS_Results_v2]( @facility_id, @unit, null, @coord_type, @matrix)'
+		select  47,'generic','tbl_SV',  'mg/m3', 'GS', 'N83SPCA III Ft', 'Select  *  INTO [EQuIS_Reporting].',' FROM [EQuIS].[rpt].[fn_HAI_EQUIS_Results_v3]( @facility_id, @unit, null, @coord_type, @matrix)'
 		union
-		select  47,'generic','tbl_IA',  'mg/m3', 'AA|IA', 'N83SPCA III Ft', 'Select  *  INTO [EQuIS_Reporting].',' FROM [EQuIS].[rpt].[fn_HAI_EQUIS_Results_v2]( @facility_id, @unit, null, @coord_type, @matrix)'
+		select  47,'generic','tbl_IA',  'mg/m3', 'AA|IA', 'N83SPCA III Ft', 'Select  *  INTO [EQuIS_Reporting].',' FROM [EQuIS].[rpt].[fn_HAI_EQUIS_Results_v3]( @facility_id, @unit, null, @coord_type, @matrix)'
 		union
 		select   
 			47
@@ -38,16 +37,11 @@ union
 			, null
 			, 'N83SPCA III Ft'
 			, 'SELECT * INTO [EQuIS_Reporting].'
-			,'FROM (' + char(10) +
-	--			'SELECT s.[sys_loc_code],s.[loc_type],s.[sys_sample_code],s.[sample_date],s.[start_depth],s.[end_depth],s.[depth_unit],' + char(10) +
-	--				   's.[basis],s.[geological_unit_code], s.[sample_type_code],s.[sample_desc],s.[sample_class],s.[remediation_status],' + char(10) +
-	--				   's.[x_coord],s.[y_coord] ,' + '''' + 'BaP EQ' + '''' + ' as [chemical_name], sl.mgp_area_type,' + char(10) +
-	--				   's.[converted_result] as [report_result_text_HA], s.[converted_result_numeric] as [result_numeric],' + char(10) +
-	--				   '((s.[converted_result] '+ ' '  + ' s.[Qualifier])) as [report_result_text_HA_qual]' + char(10) +
-	--			'FROM  ' + @SchemaName + '.tbl_SO s' + char(10) +
-	--			'left join dbo.tbl_36599_SamplingLocation sl on s.sys_loc_code = sl.sys_loc_code' + char(10) +
-	--			'WHERE [cas_rn] = ' + '''' + '50-32-8BAPEQ.RL' + ''''  + char(10) +
-	--		 ') as soilBapTable '
+			,' FROM (SELECT s.[sys_loc_code],s.[loc_type],s.[sys_sample_code],s.[sample_date],s.[start_depth],s.[end_depth],s.[depth_unit], s.[basis],s.[geological_unit_code], s.[sample_type_code],s.[sample_desc],s.[sample_class],s.[remediation_status],
+				   s.[x_coord],s.[y_coord] ,' + '''' + 'BaP EQ' + '''' + ' as [chemical_name], sl.mgp_area_type,
+					   s.[converted_result] as [report_result_text_HA], s.[converted_result_numeric] as [result_numeric],
+					   ((s.[converted_result] '+ '+ ' + '''' + ' ' + '''' + ' + '  + ' s.[Qualifier])) as [report_result_text_HA_qual]
+						FROM  s_36599_test.tbl_SO s left join dbo.tbl_36599_SamplingLocation sl on s.sys_loc_code = sl.sys_loc_code WHERE [cas_rn] = ' + '''' + '50-32-8BAPEQ.RL' + '''' + ') as soilBapTable '
 
 
 		declare @t table (table_name varchar (30))
