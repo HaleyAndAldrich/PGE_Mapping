85ee71911020829aa04cbd425882bab031814c58
HaleyAndAldrich <dhiggins@haleyaldrich.com>
2017-04-24 12:06:16 -0700

    add to repo
    Allows Enterprise to query local equis web data status report

---
+++ create sp_hai_web_date_enterprise_report.sql
@@ -0,0 +1,23 @@
+use equis
+go
+
+
+Create procedure hai.sp_hai_web_date_enterprise_report
+as 
+begin
+
+	select 
+	el1.etl_id
+	,cast(el1.run_date as smalldatetime) rundate 
+	,el1.schema_name
+	,el1.table_name
+	,el1.row_count
+	,el1.status
+	,el1.row_count - el2.row_count as row_count_change
+	
+	from ##etl_log el1
+	left join ##etl_log el2 
+	on el1.etl_id-1 = el2.etl_id and el1.table_name = el2.table_name
+	order by el1.etl_id desc
+end
+	
