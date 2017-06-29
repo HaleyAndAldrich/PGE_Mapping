5e8aa59003f17994aaf7a786dd255c445b4d8024
HaleyAndAldrich <dhiggins@haleyaldrich.com>
2017-06-29 13:04:45 -0700

    add new location table

--- populate tbl_custom_tables PSEG.sql
+++ populate tbl_custom_tables PSEG.sql
@@ -7,14 +7,23 @@ go
 
 
 delete tbl_custom_tables where facility_id =  5036806
+
 insert into tbl_custom_tables
-		select 5036806,'generic','tbl_All_Results',  null, 'SO','N83SP NJ Ft', 'Select * INTO [EQuIS_Reporting].','  FROM [EQuIS].[rpt].[fn_HAI_EQUIS_Results_v3]( @facility_id, @unit, null, @coord_type, @matrix)'
+select 5036806,'generic','tbl_All_Results',  null, 'SO','N83SP NJ Ft', 'Select * INTO [EQuIS_Reporting].','  FROM [EQuIS].[rpt].[fn_HAI_EQUIS_Results_v3]( @facility_id, @unit, null, @coord_type, @matrix)'
+	union
+select   
+	5036806
+	,'location'
+	,'tbl_location'
+	, null
+	, null
+	, 'N83SP NJ Ft'
+	, 'SELECT distinct[subfacility_name],[sys_loc_code],[loc_name],[loc_type],[task_code],[reference_elevation],[reference_elevation_point],[coord_type_code],[x_coord],[y_coord],[SRID],[APN],Sharepoint_URL into '
+	,' FROM [EQuIS_Reporting].[s_s_129402E002].[tbl_Results] where (permission_type_code = 0 and permission_type_code is not null) or task_code = ' + '''' + 'none' + '''' 
 
 
-		declare @t table (table_name varchar (30))
-		insert into @t select table_name from tbl_custom_tables
 
-		select * from tbl_custom_tables
+select * from tbl_custom_tables
 
 
 
