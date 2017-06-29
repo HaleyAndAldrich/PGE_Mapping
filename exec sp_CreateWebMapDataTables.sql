df5d87a531a63ea3059f457d6b1099165f11e892
HaleyAndAldrich <dhiggins@haleyaldrich.com>
2017-06-29 13:46:16 -0700

    add sharepoint YN support

--- exec sp_CreateWebMapDataTables.sql
+++ exec sp_CreateWebMapDataTables.sql
@@ -1,9 +1,20 @@
 
 
 
-exec equis_reporting.dbo.sp_CreateWebMapDataTables 
+exec equis_reporting.dbo.sp_CreateWebMapDataTables
 	47
 	,'s_36599E00'
 	,'N83SPCA III Ft'
 	, N'2227'
+	,'NAVD88'
 	,'tbl_36599_SamplingLocation'
+	,'Y'
+
+	exec equis_reporting.dbo.sp_CreateWebMapDataTables
+	5036806
+	,'s_129402E002'
+	,'N83SP NJ Ft'
+	, N'102711'
+	, null
+	,null  --sample location
+	,'n'
