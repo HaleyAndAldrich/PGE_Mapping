3d84409892c6f0fc5018d5cab7b7db5c2cee6bfb
HaleyAndAldrich <dhiggins@haleyaldrich.com>
2017-07-12 15:21:53 -0700

    updates

--- exec sp_CreateWebMapDataTables.sql
+++ exec sp_CreateWebMapDataTables.sql
@@ -3,18 +3,18 @@
 
 exec equis_reporting.dbo.sp_CreateWebMapDataTables
 	47
-	,'s_36599E00'
+	,'s_36599E00_test'
 	,'N83SPCA III Ft'
 	, N'2227'
 	,'NAVD88'
 	,'tbl_36599_SamplingLocation'
 	,'Y'
 
-	exec equis_reporting.dbo.sp_CreateWebMapDataTables
-	5036806
-	,'s_129402E002'
-	,'N83SP NJ Ft'
-	, N'102711'
-	, null
-	,null  --sample location
-	,'n'
+	--exec equis_reporting.dbo.sp_CreateWebMapDataTables
+	--5036806
+	--,'s_129402E002'
+	--,'N83SP NJ Ft'
+	--, N'102711'
+	--, null
+	--,null  --sample location
+	--,'n'
