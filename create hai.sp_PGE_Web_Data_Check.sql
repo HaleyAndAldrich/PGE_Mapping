7bd21edb3e4fa283dd37b4c5242d0eb0edcfe884
HaleyAndAldrich <dhiggins@haleyaldrich.com>
2017-10-17 08:39:05 -0700

    hai.sp_PGE_Web_Data_Check   add to repo
    Compares EQuIS Data to EQuIS_Reporting Data available on PGE Web Map

---
+++ create hai.sp_PGE_Web_Data_Check.sql
@@ -0,0 +1,41 @@
+
+use equis
+go
+
+set nocount on
+go
+
+/*Compares EQuIS Data to EQuIS_Reporting Data available on PGE Web Map*/
+
+alter procedure hai.sp_PGE_Web_Data_Check 
+	(@facility_id int)
+	as
+	begin
+		select distinct
+			z.task_code
+			,z.permission_type_code
+			,case when y.task_code is not null then 'On GIS Server' else '--' end as 'Status'
+		from (
+			select distinct
+			ld.apn
+			,ld.address
+			,s.task_code
+			,tp.permission_type_code
+			from dt_hai_location_details ld
+			inner join dt_sample s on ld.facility_id = s.facility_id and ld.sys_loc_code = s.sys_loc_code
+			left join dt_hai_task_permissions tp on s.facility_id = tp.facility_id and s.task_code = tp.task_code
+			where s.facility_id = @facility_id and s.task_code not like '%idw%'
+			)z
+
+		left join 
+
+			(select distinct
+				task_code
+				,[APN]
+				,[permission_type_code]
+
+			  FROM [EQuIS_Reporting].[s_36599E00].[tbl_Results]
+			 )y
+			  on z.task_code = y.task_code
+			  order by z.task_code
+		end
